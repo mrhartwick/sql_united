68635e6bb2e8f399f77d0ca1c1c530ab9a347455
matthew.hartwick@NYCMECMHARTWICK <mrhartwick@gmail.com>
2017-03-10 15:31:25 -0500

    replaced lag with sum over to account for placements that dont come in pairs ghost and real

--- saved_proc/sql_server/crt_dbm_cost.sql
+++ saved_proc/sql_server/crt_dbm_cost.sql
@@ -1,119 +1,130 @@
--- This code is wrong - the lag() calls assume that there are two rows for every placement ID. Sometimes there's 1; sometimes there are 3.
-
 alter procedure dbo.crt_dbm_cost
 as
 if OBJECT_ID('master.dbo.dbm_cost',N'U') is not null
-    drop table master.dbo.dbm_cost;
+  drop table master.dbo.dbm_cost;
 
 
 create table master.dbo.dbm_cost
 (
-dcmDate      int            not null,
-campaign     nvarchar(4000) not null,
-campaign_id  int            not null,
-plce_id      nvarchar(6)    not null,
-placement    nvarchar(4000) not null,
-placement_id int            not null,
-dbm_cost     decimal(20,10) not null,
-imps         int            not null,
-clicks       int            not null,
-vew_con     int             not null,
-clk_con     int             not null,
-con         int             not null,
-vew_tix     int             not null,
-clk_tix     int             not null,
-tix         int             not null,
-vew_rev     decimal(20,10)  not null,
-clk_rev     decimal(20,10)  not null,
-rev         decimal(20,10)  not null
+  dcmDate      int            not null,
+  campaign     nvarchar(4000) not null,
+  campaign_id  int            not null,
+  plce_id      nvarchar(6)    not null,
+  placement    nvarchar(4000) not null,
+  placement_id int            not null,
+  dbm_cost     decimal(20,10) not null,
+  imps         int            not null,
+  clicks       int            not null,
+  vew_con      int            not null,
+  clk_con      int            not null,
+  con          int            not null,
+  vew_tix      int            not null,
+  clk_tix      int            not null,
+  tix          int            not null,
+  vew_rev      decimal(20,10) not null,
+  clk_rev      decimal(20,10) not null,
+  rev          decimal(20,10) not null
 );
 
 insert into master.dbo.dbm_cost
 
-select
-    t2.dcmmatchdate                                     as dcmdate,
-    t2.campaign                                         as campaign,
-    t2.campaign_id                                      as campaign_id,
-    t2.plce_id                                          as plce_id,
-    t2.placement                                        as placement,
-    t2.placement_id                                     as placement_id,
-    sum(t2.dbm_cost)                                    as dbm_cost,
-    sum(t2.imps)                                        as imps,
-    sum(t2.clicks)                                      as clicks,
-    isNull(sum(t2.vew_con),cast(0 as int))              as vew_con,
-    isNull(sum(t2.clk_con),cast(0 as int))              as clk_con,
-    isNull(sum(t2.con),cast(0 as int))                  as con,
-    isNull(sum(t2.vew_tix) ,cast(0 as int))             as vew_tix,
-    isNull(sum(t2.clk_tix) ,cast(0 as int))             as clk_tix,
-    isNull(sum(t2.tix),cast(0 as int))                  as tix,
-    isNull(sum(t2.vew_rev) ,cast(0 as decimal(20,10)))  as vew_rev,
-    isNull(sum(t2.clk_rev) ,cast(0 as decimal(20,10)))  as clk_rev,
-    isNull(sum(t2.rev)     ,cast(0 as decimal(20,10)))  as rev
+  select
+    t2.dcmmatchdate                                   as dcmdate,
+    t2.campaign                                       as campaign,
+    t2.campaign_id                                    as campaign_id,
+    t2.plce_id                                        as plce_id,
+    t2.placement                                      as placement,
+    t2.placement_id                                   as placement_id,
+    sum(t2.dbm_cost)                                  as dbm_cost,
+    sum(t2.imps)                                      as imps,
+    sum(t2.clicks)                                    as clicks,
+    isNull(sum(t2.vew_con),cast(0 as int))            as vew_con,
+    isNull(sum(t2.clk_con),cast(0 as int))            as clk_con,
+    isNull(sum(t2.con),cast(0 as int))                as con,
+    isNull(sum(t2.vew_tix),cast(0 as int))            as vew_tix,
+    isNull(sum(t2.clk_tix),cast(0 as int))            as clk_tix,
+    isNull(sum(t2.tix),cast(0 as int))                as tix,
+    isNull(sum(t2.vew_rev),cast(0 as decimal(20,10))) as vew_rev,
+    isNull(sum(t2.clk_rev),cast(0 as decimal(20,10))) as clk_rev,
+    isNull(sum(t2.rev),cast(0 as decimal(20,10)))     as rev
 
-from (
-select
-    t1.dcmdate,
-    t1.dcmmatchdate,
-    t1.campaign,
-    t1.campaign_id,
-    t1.site_dcm,
-    t1.site_id_dcm,
-    t1.plce_id,
+  from (
+         select
+           t1.dcmdate,
+           t1.dcmmatchdate,
+           t1.campaign,
+           t1.campaign_id,
+           t1.site_dcm,
+           t1.site_id_dcm,
+           t1.plce_id,
 --     t1.lagplcnbr,
-    t1.placement,
-    t1.placement_id,
-    sum(t1.lag_dbm_cost)                                                          as dbm_cost,
-    sum(t1.impressions)                                                           as imps,
-    sum(t1.clicks)                                                                as clicks,
-    sum(t1.vew_con) + sum(t1.lag_vew_con)                                         as vew_con,
-    sum(t1.clk_con) + sum(t1.lag_clk_con)                                         as clk_con,
-    sum(t1.vew_con) + sum(t1.lag_vew_con) + sum(t1.clk_con) + sum(t1.lag_clk_con) as con,
-    sum(t1.vew_tix) + sum(t1.lag_vew_tix)                                         as vew_tix,
-    sum(t1.clk_tix) + sum(t1.lag_clk_tix)                                         as clk_tix,
-    sum(t1.vew_tix) + sum(t1.lag_vew_tix) + sum(t1.clk_tix) + sum(t1.lag_clk_tix) as tix,
-    sum(t1.vew_rev) + sum(t1.lag_vew_rev)                                         as vew_rev,
-    sum(t1.clk_rev) + sum(t1.lag_clk_rev)                                         as clk_rev,
-    sum(t1.rev) + sum(t1.lag_rev)                                                 as rev
+           t1.placement,
+           t1.placement_id,
+           t1.dbm_cost                                                                                              as dbm_cost1,
+           sum(t1.dbm_cost) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as dbm_cost,
+           t1.impressions                                                                                           as imps,
+           t1.clicks                                                                                                as clicks,
+           t1.vew_con                                                                                               as vew_con1,
+           sum(t1.vew_con) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as vew_con,
+           t1.clk_con                                                                                               as clk_con1,
+           sum(t1.clk_con) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as clk_con,
+           t1.con                                                                                                   as con1,
+           sum(t1.con) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as con,
+           t1.vew_tix                                                                                               as vew_tix1,
+           sum(t1.vew_tix) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as vew_tix,
+           t1.clk_tix                                                                                               as clk_tix1,
+           sum(t1.clk_tix) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as clk_tix,
+           t1.tix                                                                                                   as tix1,
+           sum(t1.tix) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as tix,
+           t1.vew_rev                                                                                               as vew_rev1,
+           sum(t1.vew_rev) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as vew_rev,
+           t1.clk_rev                                                                                               as clk_rev1,
+           sum(t1.clk_rev) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as clk_rev,
+           t1.rev                                                                                                   as rev1,
+           sum(t1.rev) over (partition by t1.dcmdate,t1.plce_id
+             order by t1.dcmdate asc,t1.plce_id asc,t1.campaign_id desc range between unbounded preceding and current row) as rev
 
 
-from (select
-          dcmreport.dcmdate                                                                    as dcmdate,
-          cast(month(cast(dcmreport.dcmdate as date)) as int)                                  as dcmmonth,
-          [dbo].udf_dateToInt(dcmreport.dcmdate)                                               as dcmmatchdate,
-          dcmreport.campaign                                                                   as campaign,
-          dcmreport.campaign_id                                                                as campaign_id,
-          dcmreport.site_dcm                                                                   as site_dcm,
-          dcmreport.site_id_dcm                                                                as site_id_dcm,
-          case when dcmReport.plce_id in ('PBKB7J','PBKB7H','PBKB7K') then 'PBKB7J'
-          else dcmReport.plce_id end                                                           as plce_id,
-          lag(dcmReport.plce_id,1,0) over (
-              order by dcmreport.dcmdate asc,dcmreport.plce_id asc,dcmreport.site_id_dcm desc) as lagplcnbr,
-          case when dcmReport.placement like 'PBKB7J%' or dcmReport.placement like 'PBKB7H%' or dcmReport.placement like 'PBKB7K%' or dcmReport.placement = 'United 360 - Polaris 2016 - Q4 - Amobee' then 'PBKB7J_UAC_BRA_016_Mobile_AMOBEE_Video360_InViewPackage_640x360_MOB_MOAT_Fixed Placement_Other_P25-54_1 x 1_Standard_Innovid_PUB PAID'
-          else dcmReport.placement end                                                         as placement,
-          dcmreport.placement_id                                                               as placement_id,
-          sum(dcmreport.dbm_cost)                                                              as dbm_cost,
-          lag(sum(dcmreport.dbm_cost),1,0) over ( order by dcmreport.dcmdate asc,dcmreport.plce_id asc,dcmreport.site_id_dcm desc) as lag_dbm_cost,
-          sum(dcmreport.impressions)                                                           as impressions,
-          sum(dcmreport.clicks)                                                                as clicks,
-          sum(dcmreport.vew_conv)                                                                  as vew_con,
-          lag(sum(dcmreport.vew_conv),1,0) over ( order by dcmreport.dcmdate asc,dcmreport.plce_id asc,dcmreport.site_id_dcm desc) as lag_vew_con,
-          sum(dcmreport.clk_conv)                                                                  as clk_con,
-          lag(sum(dcmreport.clk_conv),1,0) over ( order by dcmreport.dcmdate asc,dcmreport.plce_id asc,dcmreport.site_id_dcm desc) as lag_clk_con,
-          sum(dcmreport.vew_tix)                                                                   as vew_tix,
-          lag(sum(dcmreport.vew_tix),1,0) over ( order by dcmreport.dcmdate asc,dcmreport.plce_id asc,dcmreport.site_id_dcm desc) as lag_vew_tix,
-          sum(dcmreport.clk_tix)                                                                   as clk_tix,
-          lag(sum(dcmreport.clk_tix),1,0) over ( order by dcmreport.dcmdate asc,dcmreport.plce_id asc,dcmreport.site_id_dcm desc) as lag_clk_tix,
-          sum(dcmreport.vew_rev)                                                                   as vew_rev,
-          lag(sum(dcmreport.vew_rev),1,0) over ( order by dcmreport.dcmdate asc,dcmreport.plce_id asc,dcmreport.site_id_dcm desc) as lag_vew_rev,
-          sum(dcmreport.clk_rev)                                                                   as clk_rev,
-          lag(sum(dcmreport.clk_rev),1,0) over ( order by dcmreport.dcmdate asc,dcmreport.plce_id asc,dcmreport.site_id_dcm desc) as lag_clk_rev,
-          sum(dcmreport.rev)                                                                   as rev,
-          lag(sum(dcmreport.rev),1,0) over ( order by dcmreport.dcmdate asc,dcmreport.plce_id asc,dcmreport.site_id_dcm desc) as lag_rev
+         from (select
+                 dcmreport.dcmdate                                   as dcmdate,
+                 cast(month(cast(dcmreport.dcmdate as date)) as int) as dcmmonth,
+                 [dbo].udf_dateToInt(dcmreport.dcmdate)              as dcmmatchdate,
+                 dcmreport.campaign                                  as campaign,
+                 dcmreport.campaign_id                               as campaign_id,
+                 dcmreport.site_dcm                                  as site_dcm,
+                 dcmreport.site_id_dcm                               as site_id_dcm,
+                 case when dcmReport.plce_id in ('PBKB7J','PBKB7H','PBKB7K') then 'PBKB7J'
+                 else dcmReport.plce_id end                          as plce_id,
+                 case when dcmReport.placement like 'PBKB7J%' or dcmReport.placement like 'PBKB7H%' or dcmReport.placement like 'PBKB7K%' or dcmReport.placement = 'United 360 - Polaris 2016 - Q4 - Amobee' then 'PBKB7J_UAC_BRA_016_Mobile_AMOBEE_Video360_InViewPackage_640x360_MOB_MOAT_Fixed Placement_Other_P25-54_1 x 1_Standard_Innovid_PUB PAID'
+                 else dcmReport.placement end                        as placement,
+                 dcmreport.placement_id                              as placement_id,
+                 sum(dcmreport.dbm_cost)                             as dbm_cost,
+                 sum(dcmreport.impressions)                          as impressions,
+                 sum(dcmreport.clicks)                               as clicks,
+                 sum(dcmreport.vew_conv)                             as vew_con,
+                 sum(dcmreport.clk_conv)                             as clk_con,
+                 sum(dcmreport.clk_conv) + sum(dcmreport.vew_conv)   as con,
+                 sum(dcmreport.vew_tix)                              as vew_tix,
+                 sum(dcmreport.clk_tix)                              as clk_tix,
+                 sum(dcmreport.clk_tix) + sum(dcmreport.vew_tix)     as tix,
+                 sum(dcmreport.vew_rev)                              as vew_rev,
+                 sum(dcmreport.clk_rev)                              as clk_rev,
+                 sum(dcmreport.rev)                                  as rev
 
-      -- openquery function call must not exceed 8,000 characters; no room for comments inside the function
-      from (
-               select *
-               from openquery(verticaunited,'
+
+               -- openquery function call must not exceed 8,000 characters; no room for comments inside the function
+               from (
+                      select *
+                      from openquery(verticaunited,'
 select
 cast(r1.date as date)    as dcmdate,
 campaign.campaign            as campaign,
@@ -161,7 +172,7 @@ from
 (
 select *
 from diap01.mec_us_united_20056.dfa2_activity
-where cast(timestamp_trunc(to_timestamp(interaction_time / 1000000),''SS'') as date) >= ''2016-07-15''
+where cast(timestamp_trunc(to_timestamp(interaction_time / 1000000),''SS'') as date) = ''2016-07-15''
 and not regexp_like(substring(other_data,(instr(other_data,''u3='') + 3),5),''mil.*'',''ib'')
 and total_revenue != 0
 and total_conversions != 0
@@ -206,7 +217,7 @@ cast((sum(dbm_media_cost_usd) / 1000000000) as decimal(20,10))            as dbm
 from (
 select *
 from diap01.mec_us_united_20056.dfa2_impression
-where cast(timestamp_trunc(to_timestamp(event_time / 1000000),''SS'') as date) >= ''2016-07-15''
+where cast(timestamp_trunc(to_timestamp(event_time / 1000000),''SS'') as date) = ''2016-07-15''
 and (site_id_dcm = 1578478 or site_id_dcm = 2202011)
 and (advertiser_id <> 0)
 -- and dbm_advertiser_id = 649134
@@ -241,7 +252,7 @@ from (
 
 select *
 from diap01.mec_us_united_20056.dfa2_click
-where cast(timestamp_trunc(to_timestamp(event_time / 1000000),''SS'') as date) >= ''2016-07-15''
+where cast(timestamp_trunc(to_timestamp(event_time / 1000000),''SS'') as date) = ''2016-07-15''
 and (advertiser_id <> 0)
 and (site_id_dcm = 1578478 or site_id_dcm = 2202011)
 -- and dbm_advertiser_id = 649134
@@ -313,7 +324,7 @@ cast(r1.date as date)
 ,placements.placement
 ')
 
-           ) as dcmreport
+                    ) as dcmreport
 
 
 -- left join
@@ -322,35 +333,36 @@ cast(r1.date as date)
 -- where prisma.costmethod != 'Flat'
 -- and prisma.cost_id = 'P8FSSK'
 
-group by
-    dcmreport.dcmdate
-    ,cast(month(cast(dcmreport.dcmdate as date)) as int)
-    ,dcmreport.campaign
-    ,dcmreport.campaign_id
-    ,dcmreport.site_dcm
-    ,dcmreport.site_id_dcm
-    ,dcmreport.plce_id
-    ,dcmreport.placement
-    ,dcmreport.placement_id
+         group by
+           dcmreport.dcmdate
+           ,cast(month(cast(dcmreport.dcmdate as date)) as int)
+           ,dcmreport.campaign
+           ,dcmreport.campaign_id
+           ,dcmreport.site_dcm
+           ,dcmreport.site_id_dcm
+           ,dcmreport.plce_id
+           ,dcmreport.placement
+           ,dcmreport.placement_id
 
-     ) as t1
+              ) as t1
 
-where t1.site_id_dcm = 1578478
 
-group by
-    t1.dcmdate,
-    t1.dcmmatchdate,
-    t1.campaign,
-    t1.campaign_id,
-    t1.site_dcm,
-    t1.site_id_dcm,
-    t1.plce_id,
-    t1.placement,
-    t1.placement_id
+--
+-- group by
+--     t1.dcmdate,
+--     t1.dcmmatchdate,
+--     t1.campaign,
+--     t1.campaign_id,
+--     t1.site_dcm,
+--     t1.site_id_dcm,
+--     t1.plce_id,
+--     t1.placement,
+--     t1.placement_id
 
-     ) as t2
+       ) as t2
 
-group by
+  where t2.site_id_dcm = 1578478
+  group by
     t2.dcmmatchdate,
     t2.campaign,
     t2.campaign_id,
