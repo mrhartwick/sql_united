a17ad144a3e6cf1876fa73d346de9d0e825a3c20
matthew.hartwick@NYCMECMHARTWICK <mrhartwick@gmail.com>
2017-03-27 11:29:14 -0400

    final version of code

--- adhoc/frequency analysis/doug_sample_code_edit.sql
+++ adhoc/frequency analysis/doug_sample_code_edit.sql
@@ -1,129 +1,146 @@
+create table diap01.mec_us_united_20056.ual_freq_tbl1
 (
-select
+    user_id        varchar(50)    not null,
+    conversiontime int            not null,
+    impressiontime int            not null,
+    revenue        decimal(20,10) not null,
+    cost           decimal(20,10) not null,
+    cvr_nbr        int            not null,
+    imp_nbr        int            not null
+);
 
+insert into diap01.mec_us_united_20056.ual_freq_tbl1
+(user_id, conversiontime, revenue, cost, impressiontime, cvr_nbr, imp_nbr)
 
-
-
-
-    from
-        (select
-            t1.user_id,
-            t1.placement_id,
-            t1.campaign_id,
-            t1.placement_id,
-            p1.plce_id,
-            t1.site_id_dcm,
-            case when d1.site_dcm like 'Google%' then 1 else 2 end as site_rank,
-            t1.conversiontime,
-            t1.revenue,
-            t1.cost,
-            t1.impressiontime,
-            t1.cvr_nbr,
-            t1.imp_nbr
-
-
+    (
+        select
+            a.user_id,
+            conversiontime,
+            revenue,
+            cost,
+            impressiontime,
+            cvr_nbr,
+            imp_nbr
         from
             (select
-                a.user_id,
-                a.campaign_id,
-                a.placement_id,
---              left(p1.placement,6) as plce_id,
-                a.site_id_dcm,
-                conversiontime,
-                revenue,
-                cost,
-                impressiontime,
-                cvr_nbr,
-                imp_nbr
-            from
-                (select
-                    user_id,
-                    site_id_dcm,
-                    campaign_id,
-                    placement_id,
-                    (t1.total_revenue * 1000000) / rates.exchange_rate       as revenue,
+                 user_id,
+                 (t1.total_revenue * 1000000) / rates.exchange_rate as revenue,
 --              cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) as conversiontime,
-                    timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as conversiontime,
---               interaction_time,
-                    row_number() over ()                                     as cvr_nbr
-                from
-                    diap01.mec_us_united_20056.dfa2_activity as t1
+--               timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as conversiontime,
+                 event_time as conversiontime,
+                 row_number() over() as cvr_nbr
+             from
+                 diap01.mec_us_united_20056.dfa2_activity as t1
 
-                    left join diap01.mec_us_mecexchangerates_20067.exchange_rates as rates
-                    on upper(substring(other_data,(instr(other_data,'u3=') + 3),3)) = upper(rates.currency)
-                        and cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) = rates.date
+                 left join diap01.mec_us_mecexchangerates_20067.exchange_rates as rates
+                     on upper(substring(other_data,(instr(other_data,'u3=') + 3),3)) = upper(rates.currency)
+                     and cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) = rates.date
 
-                where cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) between '2016-07-15' and '2016-07-15'
-                    and not regexp_like(substring(other_data,(instr(other_data,'u3=') + 3),5),'mil.*','ib')
-                    and total_revenue <> 0
-                    and total_conversions <> 0
-                    and activity_id = 978826
-                    and (advertiser_id <> 0)
-                    and user_id <> '0'
-                    and (campaign_id = 9639387 or campaign_id = 8958859)
-                    and (site_id_dcm = 1578478 or site_id_dcm = 2202011)
+             where cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) between '2016-07-15' and '2016-12-31'
+                 and not regexp_like(substring(other_data,(instr(other_data,'u3=') + 3),5),'mil.*','ib')
+                 and total_revenue <> 0
+                 and total_conversions <> 0
+                 and activity_id = 978826
+                 and (advertiser_id <> 0)
+                 and user_id <> '0'
+                 and (campaign_id = 9639387 or campaign_id = 8958859)
+                 and (site_id_dcm = 1578478 or site_id_dcm = 2202011)
 
-                ) as a,
-                (select
-                    user_id,
-                    site_id_dcm,
-                    campaign_id,
-                    placement_id,
-                    timestamp_trunc(to_timestamp(event_time / 1000000),'SS')  as impressiontime,
+            ) as a,
+            (select
+                 user_id,
+--               timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as impressiontime,
+                event_time as impressiontime,
 --              cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) as impressiontime,
-                    cast((dbm_media_cost_usd / 1000000000) as decimal(20,10)) as cost,
-                    row_number() over ()                                      as imp_nbr
-                from
-                    diap01.mec_us_united_20056.dfa2_impression
+                 cast((dbm_media_cost_usd / 1000000000) as decimal (20,10)) as cost,
+                 row_number() over() as imp_nbr
+             from
+                 diap01.mec_us_united_20056.dfa2_impression
 
-                where cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) between '2016-07-15' and '2016-07-15'
-                    and (campaign_id = 9639387 or campaign_id = 8958859)
-                    and (site_id_dcm = 1578478 or site_id_dcm = 2202011)
-                    and user_id <> '0'
-                    and (advertiser_id <> 0)
-                ) as b
-            where
+             where cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) between '2016-07-15' and '2016-12-31'
+                 and (campaign_id = 9639387 or campaign_id = 8958859)
+                 and (site_id_dcm = 1578478 or site_id_dcm = 2202011)
+                 and user_id <> '0'
+                 and (advertiser_id <> 0)
+            ) as b
+        where
+            a.user_id = b.user_id and
+                conversiontime > impressiontime
+);
+commit;
+-- ====================================================================================================================
+create table diap01.mec_us_united_20056.ual_freq_tbl2
+(
+    user_id    varchar(50)    not null,
+    cvr_nbr    int            not null,
+    cvr_credit decimal(20,10) not null
+
+);
+
+insert into diap01.mec_us_united_20056.ual_freq_tbl2
+(user_id, cvr_nbr, cvr_credit)
+
+    (select
+                user_id,
+                cvr_nbr,
+                1/count(distinct imp_nbr) as cvr_credit
+from
+                diap01.mec_us_united_20056.ual_freq_tbl1
+group by
+1,2);
+commit;
+
+-- ====================================================================================================================
+
+create table diap01.mec_us_united_20056.ual_freq_tbl3
+(
+    user_id        varchar(50)    not null,
+    conversiontime int            not null,
+    impressiontime int            not null,
+    revenue        decimal(20,10) not null,
+    cost           decimal(20,10) not null,
+    cvr_nbr        int            not null,
+    imp_nbr        int            not null,
+    yr             int            not null,
+    wk             int            not null,
+    cvr_credit     decimal(20,10) not null
+
+);
+
+insert into diap01.mec_us_united_20056.ual_freq_tbl3
+(user_id, conversiontime, impressiontime, revenue, cost, cvr_nbr, imp_nbr, yr, wk, cvr_credit)
+
+    (select
+                a.user_id,
+                a.conversiontime,
+                a.impressiontime,
+                a.revenue,
+                a.cost,
+                a.cvr_nbr,
+                a.imp_nbr,
+                date_part('year', cast(timestamp_trunc(to_timestamp(a.impressiontime / 1000000),'SS') as date)) as wk,
+                date_part('week', cast(timestamp_trunc(to_timestamp(a.impressiontime / 1000000),'SS') as date)) as wk,
+                b.cvr_credit
+from
+                diap01.mec_us_united_20056.ual_freq_tbl1 a,
+                diap01.mec_us_united_20056.ual_freq_tbl2 b
+where
                 a.user_id = b.user_id and
-                    conversiontime > impressiontime
---                  and a.site_id_dcm = b.site_id_dcm
---                  and a.campaign_id = b.campaign_id
-                    and a.placement_id = b.placement_id
-
-            ) as t1
-
-            left join
-            (
-                select
-                    t0.plce_id,
-                    t0.placement_id,
-                    t0.campaign_id,
-                    t0.site_id_dcm
-
-                from (select
-                    campaign_id                                           as campaign_id,
-                    site_id_dcm                                           as site_id_dcm,
-                    placement_id                                          as placement_id,
-                    left(placement,6)                                             as plce_id,
-                    cast(placement_start_date as date)                    as thisdate,
-                    row_number() over (partition by campaign_id,site_id_dcm,placement_id
-                        order by cast(placement_start_date as date) desc) as r1
-                from diap01.mec_us_united_20056.dfa2_placements
-                    where (campaign_id = 9639387 or campaign_id = 8958859)
-                    and (site_id_dcm = 1578478 or site_id_dcm = 2202011)
-                     ) as t0
-                where r1 = 1
-            ) as p1
-            on t1.placement_id = p1.placement_id
-                and t1.campaign_id = p1.campaign_id
-                and t1.site_id_dcm = p1.site_id_dcm
-            left join
-            (
-                select
-                    site_dcm,
-                    site_id_dcm
-                from diap01.mec_us_united_20056.dfa2_sites
-                where (site_id_dcm = 1578478 or site_id_dcm = 2202011)
-            ) as d1
-            on t1.site_id_dcm = d1.site_id_dcm
-        ) as t2
+                a.cvr_nbr = b.cvr_nbr
     );
+commit;
+
+
+select
+                yr,
+                wk,
+                count(distinct user_id) as user_cnt,
+                count(distinct cvr_nbr) as cvr_cnt,
+                count(distinct imp_nbr) as imp_cnt,
+                sum(cvr_credit) as cvr_credit,
+                sum(revenue*cvr_credit) as revenue,
+                sum(cost) as cost
+from
+                diap01.mec_us_united_20056.ual_freq_tbl3
+group by
+                1,2;
