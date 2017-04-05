e708f3f084cd2e3d88e609669971cd2487e417cf
matthew.hartwick@NYCMECMHARTWICK <mrhartwick@gmail.com>
2017-04-05 09:28:24 -0400

    transfer dv_map logic into function

--- reporting/MasterQuery.sql
+++ reporting/MasterQuery.sql
@@ -1,4 +1,4 @@
---Exec Summ Report:
+
 
 --      Version incorporating capped cost and DBM cost
 --
@@ -15,12 +15,12 @@
 --
 -- exec master.dbo.crt_dv_summ go    -- crt_ separate dv aggregate table and store it in my instance; joining to the vertica table in the query
 -- exec master.dbo.crt_mt_summ go    -- crt_ separate moat aggregate table and store it in my instance; joining to the vertica table in the query
--- exec [10.2.186.148\SQLINS02,4721].dm_1161_unitedairlinesusa.dbo.crt_ivd_summTbl go
--- exec [10.2.186.148\SQLINS02,4721].DM_1161_UnitedAirlinesUSA.dbo.crt_prs_viewTbl go
+-- exec [10.2.186.148,4721].dm_1161_unitedairlinesusa.dbo.crt_ivd_summTbl go
+-- exec [10.2.186.148,4721].DM_1161_UnitedAirlinesUSA.dbo.crt_prs_viewTbl go
 --
--- exec [10.2.186.148\SQLINS02,4721].dm_1161_unitedairlinesusa.dbo.crt_prs_amttbl go
--- exec [10.2.186.148\SQLINS02,4721].dm_1161_unitedairlinesusa.dbo.crt_prs_packtbl go
--- exec [10.2.186.148\SQLINS02,4721].dm_1161_unitedairlinesusa.dbo.crt_prs_summtbl go
+-- exec [10.2.186.148,4721].dm_1161_unitedairlinesusa.dbo.crt_prs_amttbl go
+-- exec [10.2.186.148,4721].dm_1161_unitedairlinesusa.dbo.crt_prs_packtbl go
+-- exec [10.2.186.148,4721].dm_1161_unitedairlinesusa.dbo.crt_prs_summtbl go
 -- exec master.dbo.crt_dfa_flatCost_dt2 go
 -- exec master.dbo.crt_dbm_cost go
 -- exec master.dbo.crt_dfa_cost_dt2 go
@@ -30,13 +30,8 @@
 declare @report_st date
 declare @report_ed date
 --
-<<<<<<< HEAD
 set @report_ed = '2017-03-27';
 set @report_st = '2017-02-14';
-=======
-set @report_ed = '2017-03-28';
-set @report_st = '2017-01-01';
->>>>>>> a3d0c77b7352974e627119cd2c7b00736e925ddf
 
 --
 -- set @report_ed = dateadd(day, -datepart(day, getdate()), getdate());
@@ -66,22 +61,12 @@ select
 -- friendly campaign name
     [dbo].udf_campaignname(t3.campaign_id,t3.campaign)                                                 as campaign,
 -- dcm campaign id
-<<<<<<< HEAD
     t3.campaign_id,
 --campaign type: Acquisition, Branding/Routes, Added Value                                                                                       as "campaign id",
     case when campaign_id = '10742878' then 'Acquisition'
     when campaign_id = '10918234' or campaign_id = '10942240' or campaign_id = '10768497' or campaign_id = '11069476' then 'Branding/Routes'
     when campaign_id = '10740457' or campaign_id = '10812738' then 'Added Value'
     else 'non-Acquisition' end                                                                         as "campaign_type",
-=======
-    t3.campaign_id                as "campaign id",
-
--- campaign type: Acquisition, Branding/Routes, Added Value
---     case when campaign_id = '10742878' then 'Acquisition'
---     when campaign_id = '10918234' or campaign_id = '10942240' or campaign_id = '10768497' or campaign_id = '11069476' then 'Branding/Routes'
---     when campaign_id = '10740457' or campaign_id = '10812738' then 'Added Value'
---     else 'non-Acquisition' end                                                                         as "campaign_type",
->>>>>>> a3d0c77b7352974e627119cd2c7b00736e925ddf
 
 
 -- preferred, friendly site name; also corresponds to what's used in the joinkey fields across dfa, dv, and moat.
@@ -103,11 +88,7 @@ select
     t3.rate                                                                                            as rate,
     t3.planned_amt                                                                                     as "planned amt",
     t3.planned_cost                                                                                    as "planned cost",
-<<<<<<< HEAD
     t3.planned_cost / max(t3.amt_count)                                                                as planned_cost,
-=======
-    -- t3.planned_cost / max(t3.amt_count)                                                                as planned_cost,
->>>>>>> a3d0c77b7352974e627119cd2c7b00736e925ddf
     case when t3.costmethod like '[Ff]lat' then t3.flatcost / max(t3.flat_count) else sum(t3.cost) end as cost,
     sum(t3.tot_led)                                                                                    as leads,
     sum(t3.dlvrimps)                                                                                   as "delivered impressions",
@@ -126,26 +107,17 @@ select
     sum(t3.tot_rev)                                                                                    as revenue,
     sum(t3.vew_rev)                                                                                    as vew_rev,
     sum(t3.clk_rev)                                                                                    as clk_thru_rev,
-<<<<<<< HEAD
     sum(t3.billrevenue)                                                                                as "billable revenue",
-=======
---     sum(t3.billrevenue)                                                                                as "billable revenue",
->>>>>>> a3d0c77b7352974e627119cd2c7b00736e925ddf
     sum(t3.adjsrevenue)                                                                                as "adjusted (final) revenue"
 from (
 
 -- for running the code here instead of at "f3," above
 
--- declare @report_st date,
--- @report_ed date;
--- --
-<<<<<<< HEAD
--- set @report_ed = '2017-03-27';
--- set @report_st = '2017-02-14';
-=======
--- set @report_ed = '2017-03-28';
--- set @report_st = '2017-01-01';
->>>>>>> a3d0c77b7352974e627119cd2c7b00736e925ddf
+declare @report_st date,
+@report_ed date;
+--
+set @report_ed = '2017-03-27';
+set @report_st = '2017-02-14';
 
 select
     cast(t2.dcmdate as date)                                                   as dcmdate,
@@ -173,6 +145,7 @@ select
     t2.placementend                                                            as placementend,
     t2.placementstart                                                          as placementstart,
     t2.dv_map                                                                  as dv_map,
+--     t2.dv_map_2                                                                  as dv_map_2,
     t2.planned_amt                                                             as planned_amt,
     t2.planned_cost                                                            as planned_cost,
     flt.flatcost                                                               as flatcost,
@@ -334,47 +307,52 @@ select
              case when cast(month(prs.placementend) as int) - cast(month(cast(t1.dcmdate as date)) as int) <= 0 then 0
              else cast(month(prs.placementend) as int) - cast(month(cast(t1.dcmdate as date)) as int) end as diff,
 
-               case
-               -- Flat-fee, cost-per-click, CPCV, and dynamic CPM should never be subject to viewability
-               when prs.costmethod = 'Flat' or prs.costmethod = 'CPC' or prs.costmethod = 'CPCV' or prs.costmethod = 'dCPM'
-                   then 'N'
+--                case
+--                -- Flat-fee, cost-per-click, CPCV, and dynamic CPM should never be subject to viewability
+--                when prs.costmethod = 'Flat' or prs.costmethod = 'CPC' or prs.costmethod = 'CPCV' or prs.costmethod = 'dCPM'
+--                    then 'N'
+--
+--                -- Corrections to SME 2016
+--                when t1.campaign_id = '10090315' and
+--                -- Inc
+--                    (t1.site_id_dcm = '1513807' or
+--                -- Xaxis
+--                     t1.site_id_dcm = '1592652')
+--                    then 'Y'
+--
+--                -- Corrections to SFO-SIN 2016
+--                when t1.campaign_id = '9923634' and
+--                -- Business Insider
+--                   ((t1.site_id_dcm = '1534879' and prs.costmethod = 'CPM') or
+--                -- Live Intent
+--                    (t1.site_id_dcm = '1853564'))
+--                    then 'N'
+--
+--                -- FlipBoard unable to implement Moat tags; must bill off of DFA impressions
+--                when t1.site_id_dcm = '2937979' then 'N'
+--                -- All targeted marketing subject to viewability; mark "Y"
+--                when t1.campaign_id = '9639387' then 'Y'
+--
+--                -- If it's CPMV and the placement has the words "Mobile," "Video," or "Pre-Roll," then it should be in Moat
+--                when prs.CostMethod = 'CPMV' and
+--                    (t1.placement like '%[Mm][Oo][Bb][Ii][Ll][Ee]%' or
+--                     t1.placement like '%[Vv][Ii][Dd][Ee][Oo]%' or
+--                     t1.placement like '%[Pp][Rr][Ee]%[Rr][Oo][Ll][Ll]%' or
+--                -- Verve
+--                     t1.site_id_dcm = '1995643'
+--                ) then 'M'
+--
+--                -- Look for viewability flags Investment began to include in placement names 6/16.
+--                when t1.placement like '%[_]DV[_]%' then 'Y'
+--                when t1.placement like '%[_]MOAT[_]%' then 'M'
+--                when t1.placement like '%[_]NA[_]%' then 'N'
+--
+--                -- If it's CPMV and marked "N," change to "Y"
+--                when prs.costmethod = 'CPMV' and prs.DV_Map = 'N' then 'Y'
+--                else prs.DV_Map end as DV_Map,
 
-               -- Corrections to SME 2016
-               when t1.campaign_id = '10090315' and
-               -- Inc
-                   (t1.site_id_dcm = '1513807' or
-               -- Xaxis
-                    t1.site_id_dcm = '1592652')
-                   then 'Y'
-
-               -- Corrections to SFO-SIN 2016
-               when t1.campaign_id = '9923634' and
-               -- Business Insider
-                  ((t1.site_id_dcm = '1534879' and prs.costmethod = 'CPM') or
-               -- Live Intent
-                   (t1.site_id_dcm = '1853564'))
-                   then 'N'
-
-               -- FlipBoard unable to implement Moat tags; must bill off of DFA impressions
-               when t1.site_id_dcm = '2937979' then 'N'
-               -- All targeted marketing subject to viewability; mark "Y"
-               when t1.campaign_id = '9639387' then 'Y'
-
-               -- If it's CPMV and the placement has the words "Mobile," "Video," or "Pre-Roll," then it should be in Moat
-               when prs.CostMethod = 'CPMV' and
-                   (t1.placement like '%[Mm][Oo][Bb][Ii][Ll][Ee]%' or t1.placement like '%[Vv][Ii][Dd][Ee][Oo]%' or t1.placement like '%[Pp][Rr][Ee]%[Rr][Oo][Ll][Ll]%'
-               -- Verve
-                       or t1.site_id_dcm = '1995643'
-               ) then 'M'
-
-               -- Look for viewability flags Investment began to include in placement names 6/16.
-               when t1.placement like '%[_]DV[_]%' then 'Y'
-               when t1.placement like '%[_]MOAT[_]%' then 'M'
-               when t1.placement like '%[_]NA[_]%' then 'N'
-
-               -- If it's CPMV and marked "N," change to "Y"
-               when prs.costmethod = 'CPMV' and prs.DV_Map = 'N' then 'Y'
-               else prs.DV_Map end as DV_Map
+--             Because dv_map logic exists in more than one query, abstract it into function for easier updating
+               [dbo].udf_dvMap(t1.campaign_id,t1.site_id_dcm,t1.placement,prs.CostMethod,prs.dv_map) as dv_map
 
 
 
@@ -434,11 +412,7 @@ from
 (
 select *
 from diap01.mec_us_united_20056.dfa2_activity
-<<<<<<< HEAD
 where cast (timestamp_trunc(to_timestamp(interaction_time / 1000000),''SS'') as date ) between ''2017-02-14'' and ''2017-03-27''
-=======
-where cast (timestamp_trunc(to_timestamp(interaction_time / 1000000),''SS'') as date ) between ''2017-01-01'' and ''2017-03-28''
->>>>>>> a3d0c77b7352974e627119cd2c7b00736e925ddf
 and not regexp_like(substring(other_data,(instr(other_data,''u3='') + 3),5),''mil.*'',''ib'')
 and (activity_id = 978826 or activity_id = 1086066)
 -- and campaign_id in (10768497, 9801178, 10742878, 10812738, 10740457) -- display 2017
@@ -478,11 +452,7 @@ cast (timestamp_trunc(to_timestamp(ti.event_time / 1000000),''SS'') as date ) as
 from (
 select *
 from diap01.mec_us_united_20056.dfa2_impression
-<<<<<<< HEAD
 where cast (timestamp_trunc(to_timestamp(event_time / 1000000),''SS'') as date ) between ''2017-02-14'' and ''2017-03-27''
-=======
-where cast (timestamp_trunc(to_timestamp(event_time / 1000000),''SS'') as date ) between ''2017-01-01'' and ''2017-03-28''
->>>>>>> a3d0c77b7352974e627119cd2c7b00736e925ddf
 -- and campaign_id in (10768497, 9801178, 10742878, 10812738, 10740457) -- display 2017
 
 and (advertiser_id <> 0)
@@ -516,11 +486,7 @@ from (
 
 select *
 from diap01.mec_us_united_20056.dfa2_click
-<<<<<<< HEAD
 where cast (timestamp_trunc(to_timestamp(event_time / 1000000),''SS'') as date ) between ''2017-02-14'' and ''2017-03-27''
-=======
-where cast (timestamp_trunc(to_timestamp(event_time / 1000000),''SS'') as date ) between ''2017-01-01'' and ''2017-03-28''
->>>>>>> a3d0c77b7352974e627119cd2c7b00736e925ddf
 -- and campaign_id in (10768497, 9801178, 10742878, 10812738, 10740457) -- display 2017
 and (advertiser_id <> 0)
 ) as tc
@@ -584,7 +550,7 @@ cast (r1.date as date )
       left join
       (
         select *
-        from [10.2.186.148\SQLINS02,4721].dm_1161_unitedairlinesusa.[dbo].prs_summ
+        from [10.2.186.148,4721].dm_1161_unitedairlinesusa.[dbo].prs_summ
       ) as prs
         on t1.placement_id = prs.adserverplacementid
 
@@ -663,7 +629,7 @@ cast (r1.date as date )
 
   left join (
               select *
-              from [10.2.186.148\SQLINS02,4721].dm_1161_unitedairlinesusa.[dbo].ivd_summ_agg
+              from [10.2.186.148,4721].dm_1161_unitedairlinesusa.[dbo].ivd_summ_agg
               where ivdate between @report_st and @report_ed
             ) as iv
       on
@@ -679,6 +645,7 @@ group by
   ,t2.campaign_id
   ,t2.cost_id
   ,t2.dv_map
+--   ,t2.dv_map_2
   ,t2.site_dcm
   ,t2.site_id_dcm
   ,t2.packagecat
@@ -734,4 +701,4 @@ group by
 
 order by
   t3.cost_id,
-  t3.dcmdate;sq
+  t3.dcmdate;
