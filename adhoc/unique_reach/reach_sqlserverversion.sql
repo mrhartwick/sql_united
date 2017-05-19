f4448ac2052bb30a9ec9089d2fc42148dc408f4e
thahmed <tammam.ahmed@mecglobal.com>
2017-05-19 16:26:52 -0400

    missed an "and"...

---
+++ adhoc/unique_reach/reach_sqlserverversion.sql
@@ -0,0 +1,207 @@
+select
+
+  t3.campaign as campaign,
+  t3.site_dcm as site,
+  t3.campaign_id as campaign_id,
+  t3.site_id_dcm as site_id,
+  sum(case when t3.jan_17_imps > 0
+    then 1
+      else 0 end)    as jan_all_users,
+  sum(t3.jan_17_cst) as jan_17_cst,
+  sum(case when t3.feb_17_imps > 0
+    then 1
+      else 0 end)    as feb_all_users,
+  sum(t3.feb_17_cst) as feb_17_cst,
+  sum(case when t3.mar_17_imps > 0
+    then 1
+      else 0 end)    as mar_all_users,
+  sum(t3.mar_17_cst) as mar_17_cst,
+  sum(case when t3.apr_17_imps > 0
+    then 1
+      else 0 end)    as apr_all_users,
+  sum(t3.apr_17_cst) as apr_17_cst
+
+
+from (
+
+  select
+    t2.campaign,
+    t2.campaign_id,
+    t2.site_id_dcm,
+    t2.site_dcm,
+    t2.jan_17_imps,
+    t2.feb_17_imps,
+    t2.mar_17_imps,
+    t2.apr_17_imps,
+    sum(case
+        when t2.year_tim = 2017 and
+             t2.mnth_tim = 1 and
+             (t2.CostMethod = 'dCPM')
+          then t2.dbm_cost
+        when t2.year_tim = 2017 and
+             t2.mnth_tim = 1
+          then (t2.rate * cast(t2.total_imp as decimal)) / cast(1000 as decimal)
+        else 0 end) as jan_17_cst,
+    sum(case
+        when t2.year_tim = 2017 and
+             t2.mnth_tim = 2 and
+             t2.CostMethod = 'dCPM'
+          then t2.dbm_cost
+        when t2.year_tim = 2017 and
+             t2.mnth_tim = 2
+          then (t2.rate * cast(t2.total_imp as decimal)) / cast(1000 as decimal)
+        else 0 end) as feb_17_cst,
+    sum(case
+        when t2.year_tim = 2017 and
+             t2.mnth_tim = 3 and
+             t2.CostMethod = 'dCPM'
+          then t2.dbm_cost
+        when t2.year_tim = 2017 and
+             t2.mnth_tim = 3
+          then (t2.rate * cast(t2.total_imp as decimal)) / cast(1000 as decimal)
+        else 0 end) as mar_17_cst,
+    sum(case
+        when t2.year_tim = 2017 and
+             t2.mnth_tim = 4 and
+             t2.CostMethod = 'dCPM'
+          then t2.dbm_cost
+        when t2.year_tim = 2017 and
+             t2.mnth_tim = 4
+          then (t2.rate * cast(t2.total_imp as decimal)) / cast(1000 as decimal)
+        else 0 end) as apr_17_cst
+
+
+  from (
+
+         select
+           r0.rate,
+           case when t001.site_id_dcm = 2202011 or t001.site_id_dcm = 3266673 or t001.site_id_dcm = 1578478
+             then 'dCPM'
+           else r0.CostMethod end as CostMethod,
+           t1.campaign_id,
+           t1.site_id_dcm,
+           t1.placement_id,
+           t1.dbm_cost,
+           c1.campaign_id,
+           s1.site_dcm,
+           t1.user_id,
+           t1.year_tim,
+           t1.mnth_tim,
+           sum(case when t1.year_tim = 2017 and t1.mnth_tim = 1
+             then 1
+               else 0 end)        as jan_17_imps,
+           sum(case when t1.year_tim = 2017 and t1.mnth_tim = 2
+             then 1
+               else 0 end)        as feb_17_imps,
+           sum(case when t1.year_tim = 2017 and t1.mnth_tim = 3
+             then 1
+               else 0 end)        as mar_17_imps,
+           sum(case when t1.year_tim = 2017 and t1.mnth_tim = 4
+             then 1
+               else 0 end)        as apr_17_imps
+
+
+         from
+
+           (select *
+            from openQuery(verticaunited, '
+
+              select
+             t001.user_id,
+             cast(timestamp_trunc(to_timestamp(t001.event_time / 1000000),''SS'') as date) as event_time,
+             date_part(''year'', cast(timestamp_trunc(to_timestamp(t001.event_time / 1000000),''SS'') as date))  as year_tim,
+             date_part(''month'', cast(timestamp_trunc(to_timestamp(t001.event_time / 1000000),''SS'') as date)) as mnth_tim,
+             case
+--           when t001.campaign_id = 8964059 and regexp_like(p1.placement,''.*Chicago.*'',''ib'') then 11177760
+             when t001.campaign_id = 8964059 and (regexp_like(p1.placement,''PG5.*'',''ib'') or regexp_like(p1.placement,''PG6.*'',''ib'')) then 11177760
+--              when t001.campaign_id = 8964059 and regexp_like(p1.placement,''PFG.*'',''ib'') then 10942240
+--              when t001.campaign_id = 8964059 and regexp_like(p1.placement,''PG0.*'',''ib'') then 11152017
+             when t001.campaign_id = 8958859 then 10742878
+                 else t001.campaign_id end as campaign_id,
+--              t001.campaign_id,
+             case when t001.site_id_dcm = 2202011 or t001.site_id_dcm = 3266673 then 1578478 else t001.site_id_dcm end as site_id_dcm,
+             t001.placement_id,
+             count(*)                                                                    as total_imp,
+             cast((sum(t001.dbm_total_media_cost_usd) / 1000000000) as decimal(20,10)) as dbm_cost,
+             cast(c1.campaign as varchar(4000)) as campaign,
+            cast(s1.site_dcm as varchar(4000)) as site_dcm
+
+
+                       from diap01.mec_us_united_20056.dfa2_impression as t001
+
+             LEFT outer JOIN
+            diap01.mec_us_united_20056.dfa2_placements AS p1
+            ON t001.placement_id = p1.placement_id
+
+            LEFT outer JOIN
+            diap01.mec_us_united_20056.dfa2_campaigns  AS c1
+            ON t001.campaign_id = c1.campaign_id
+
+            LEFT outer JOIN
+            diap01.mec_us_united_20056.dfa2_sites AS s1
+            ON t001.site_id_dcm = s1.site_id_dcm
+
+
+         where
+             user_id <> ''0''
+                 and t001.campaign_id in (8964059, 8958859, 11177760, 11224605, 10742878)
+--                  and t001.campaign_id <> ''10698273'' -- UK Acquisition 2017
+--                  and t001.campaign_id <> ''11221036'' -- Hong Kong 2017
+--                  and t001.campaign_id <> ''10812738'' -- Marketing funds 2017
+                 and t001.advertiser_id <> ''0''
+                 and cast(timestamp_trunc(to_timestamp(t001.event_time / 1000000),''SS'') as date) between ''2017-01-01'' and ''2017-04-30''
+                  and regexp_like(c1.campaign,''.*2017.*'',''ib'')
+                  and not regexp_like(c1.campaign,''.*Search.*'',''ib'')
+                  -- and not regexp_like(c1.campaign,''.*BidManager.*'',''ib'')
+
+         group by
+             t001.user_id,
+             cast(timestamp_trunc(to_timestamp(t001.event_time / 1000000),''SS'') as date),
+             t001.campaign_id,
+             t001.site_id_dcm,
+             p1.placement,
+             t001.placement_id,
+             cast(s1.site_dcm as varchar(4000)),
+             cast(c1.campaign as varchar(4000))
+
+    ') as t1
+
+              left join (select distinct
+                           rate,
+                           costMethod
+                         from [10.2.186.148\SQLINS02,4721].DM_1161_UnitedAirlinesUSA.dbo.prs_summ) as r0
+                on t1.placement_id = r0.AdserverPlacementId
+
+
+            group by
+              r0.rate,
+              r0.CostMethod,
+              t1.campaign_id,
+              t1.site_id_dcm,
+              t1.dbm_cost,
+              c1.campaign,
+              s1.site_dcm,
+              t1.user_id,
+              t1.year_tim,
+              t1.mnth_tim,
+              t1.placement_id
+--
+           ) as t2
+
+
+         group by
+
+
+           t2.campaign,
+           t2.campaign_id,
+           t2.site_id_dcm,
+           t2.site_dcm,
+           t2.jan_17_imps,
+           t2.feb_17_imps,
+           t2.mar_17_imps,
+           t2.apr_17_imps
+
+       ) as t3
+
+  group by t3.campaign, t3.site_dcm,t3.campaign_id,
+  t3.site_id_dcm
