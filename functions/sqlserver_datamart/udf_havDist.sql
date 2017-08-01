5bc814d2f7981bb396ca004495733048b91fbc1d
matthew.hartwick@NYCMECMHARTWICK <mrhartwick@gmail.com>
2017-08-01 11:26:21 -0400

    function to determine distance between two coord sets

---
+++ functions/sqlserver_datamart/udf_havDist.sql
@@ -0,0 +1,22 @@
+-- Function to calculate distance between two sets of coordinates
+-- using the Haversine formula
+
+create function [dbo].udf_havDist(
+    @lat1 decimal(20,10),@lat2 decimal(20,10),@lon1 decimal(20,10),@lon2 decimal(20,10)
+)
+    returns decimal(20,10)
+    with execute as caller
+as
+    begin
+        declare @dist decimal(20,10)
+        set @dist =
+        (acos(cos(radians(@lat1)) * -- lat1
+                  cos(radians(@lat2)) * -- lat2
+                  cos(radians(@lon1) - -- long1
+                          radians(@lon2)) + --long2
+                  sin(radians(@lat1)) * -- lat 1
+                      sin(radians(@lat2))) -- lat2
+            * 6371000) / 1000 -- convert to kilometers;
+        return @dist
+    end
+go
