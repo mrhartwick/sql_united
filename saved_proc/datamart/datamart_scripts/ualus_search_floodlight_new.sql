9da7af2664439c36ed513d4faddfc65f1d60eb7e
matthew.hartwick@NYCMECMHARTWICK <mrhartwick@gmail.com>
2017-08-07 16:00:28 -0400

    add some replace() functions to eliminate weird characters that sometimes show up in INT fields

--- saved_proc/datamart/datamart_scripts/ualus_search_floodlight_new.sql
+++ saved_proc/datamart/datamart_scripts/ualus_search_floodlight_new.sql
@@ -12,62 +12,39 @@ WHERE  date IN (SELECT DISTINCT Cast([date] AS DATE)
 
 --transform/cast data types and insert cleansed data into the fact table
 INSERT INTO ualus_search_floodlight
-SELECT Cast(date AS DATE)                                    AS date,
-       Cast([activity id] AS NVARCHAR(50))                   activity_id,
-       -- cast([activity date/time] as date)                    activity_date,
-       Cast([placement id] AS NVARCHAR(50))                  placement_id,
-       Cast([campaign id] AS NVARCHAR(50))                   campaign_id,
-       Cast([paid search engine account id] AS NVARCHAR(50)) pdsearch_engine_account_id,
-       Cast([paid search engine account] AS NVARCHAR(1000))  pdsearch_engine_account,
-       Cast([paid search campaign id] AS NVARCHAR(50))       pdsearch_campaign_id,
-       Cast([paid search campaign] AS NVARCHAR(1000))        pdsearch_campaign,
-       Cast([paid search ad id] AS NVARCHAR(50))             pdsearch_adid,
-       Cast([paid search ad] AS NVARCHAR(1000))              pdsearch_ad,
-       Cast([paid search keyword id] AS NVARCHAR(50))        pdsearch_keywordid,
-       Cast([paid search keyword] AS NVARCHAR(1000))         pdsearch_keyword,
-       Cast([site id (dcm)] AS NVARCHAR(50))                 siteid_dcm,
-       Cast([site (dcm)] AS NVARCHAR(1000))                  site_dcm,
-       Cast([paid search advertiser id] AS NVARCHAR(50))     pdsearch_advertiser_id,
-       Cast([paid search advertiser] AS NVARCHAR(1000))      pdsearch_advertiser,
-       Cast([paid search ad group id] AS NVARCHAR(50))       pdsearch_adgroup_id,
-       Cast([paid search ad group] AS NVARCHAR(1000))        pdsearch_adgroup,
-       Cast([site id (site directory)] AS NVARCHAR(50))      siteid_sitedirectory,
-       Cast([site (site directory)] AS NVARCHAR(1000))       site_sitedirectory,
-       CASE
-         WHEN Isnumeric([revenue (string)]) = 1 THEN Cast([revenue (string)] AS DECIMAL(38, 2))
-         ELSE 0
-       END                                                   revenue_string,
-       Cast([currency (string)] AS NVARCHAR(50))             currency,
-       -- cast([pnr_base64encoded (string)] as nvarchar(50))    pnr_base64_string,
-       CASE
-         WHEN Isnumeric([number of tickets (string)]) = 1 THEN Cast([number of tickets (string)] AS INT)
-         ELSE 0
-       END                                                   number_of_tickets,
-       Cast([origin_1 (string)] AS NVARCHAR(50))             origin_1,
-       Cast([origin_2 (string)] AS NVARCHAR(50))             origin_2,
-       CASE
-         WHEN Isnumeric([total conversions]) = 1 THEN Cast([total conversions] AS INT)
-         ELSE 0
-       END                                                   total_conversions,
-       CASE
-         WHEN Isnumeric([total revenue]) = 1 THEN Cast([total revenue] AS DECIMAL(38, 2))
-         ELSE 0
-       END                                                   total_revenue,
-       Cast([transaction count] AS DECIMAL(38, 2))           transaction_count,
-       CASE
-         WHEN Isnumeric([revenue (number)]) = 1 THEN Cast([revenue (number)] AS DECIMAL(38, 2))
-         ELSE 0
-       END                                                   revenue_number,
-       -- cast([pnr_base64encoded (number)] as decimal(38, 2))  pnr_base64_number,
-       Cast([click-through revenue] AS DECIMAL(38, 2))       clickthrough_revenue,
-       CASE
-         WHEN Isnumeric([click-through conversions]) = 1 THEN Cast([click-through conversions] AS DECIMAL(20, 10))
-         ELSE 0
-       END                                                   clickthrough_conversions,
-       CASE
-         WHEN Isnumeric([click-through transaction count]) = 1 THEN Cast([click-through transaction count] AS INT)
-         ELSE 0
-       END                                                   clickthrough_transactioncount
+SELECT
+       Cast(date AS DATE)                                    AS "date",
+       Cast([activity id] AS NVARCHAR(50))                   as activity_id,
+       Cast([placement id] AS NVARCHAR(50))                  as placement_id,
+       Cast([campaign id] AS NVARCHAR(50))                   as campaign_id,
+       Cast([paid search engine account id] AS NVARCHAR(50)) as pdsearch_engine_account_id,
+       Cast([paid search engine account] AS NVARCHAR(1000))  as pdsearch_engine_account,
+       Cast([paid search campaign id] AS NVARCHAR(50))       as pdsearch_campaign_id,
+       Cast([paid search campaign] AS NVARCHAR(1000))        as pdsearch_campaign,
+       Cast([paid search ad id] AS NVARCHAR(50))             as pdsearch_adid,
+       Cast([paid search ad] AS NVARCHAR(1000))              as pdsearch_ad,
+       Cast([paid search keyword id] AS NVARCHAR(50))        as pdsearch_keywordid,
+       Cast([paid search keyword] AS NVARCHAR(1000))         as pdsearch_keyword,
+       Cast([site id (dcm)] AS NVARCHAR(50))                 as siteid_dcm,
+       Cast([site (dcm)] AS NVARCHAR(1000))                  as site_dcm,
+       Cast([paid search advertiser id] AS NVARCHAR(50))     as pdsearch_advertiser_id,
+       Cast([paid search advertiser] AS NVARCHAR(1000))      as pdsearch_advertiser,
+       Cast([paid search ad group id] AS NVARCHAR(50))       as pdsearch_adgroup_id,
+       Cast([paid search ad group] AS NVARCHAR(1000))        as pdsearch_adgroup,
+       Cast([site id (site directory)] AS NVARCHAR(50))      as siteid_sitedirectory,
+       Cast([site (site directory)] AS NVARCHAR(1000))       as site_sitedirectory,
+       CASE WHEN Isnumeric([revenue (string)]) = 1 THEN Cast([revenue (string)] AS DECIMAL(38, 2)) ELSE 0 END as revenue_string,
+       Cast([currency (string)] AS NVARCHAR(50))             as currency,
+       CASE WHEN Isnumeric([number of tickets (string)]) = 1 THEN Cast(replace(replace([number of tickets (string)],'.0','0'),'.00','0') AS INT) ELSE 0 END as number_of_tickets,
+       Cast([origin_1 (string)] AS NVARCHAR(50))             as origin_1,
+       Cast([origin_2 (string)] AS NVARCHAR(50))             as origin_2,
+       CASE WHEN Isnumeric([total conversions]) = 1 THEN Cast(replace(replace([total conversions],'.0','0'),'.00','0') AS INT) ELSE 0 END as total_conversions,
+       CASE WHEN Isnumeric([total revenue]) = 1 THEN Cast([total revenue] AS DECIMAL(38, 2)) ELSE 0 END as total_revenue,
+       CASE WHEN Isnumeric([transaction count]) = 1 THEN Cast(replace(replace([transaction count],'.0','0'),'.00','0') AS INT) ELSE 0 END as total_conversions,
+       CASE WHEN Isnumeric([revenue (number)]) = 1 THEN Cast([revenue (number)] AS DECIMAL(38, 2)) ELSE 0 END as revenue_number,
+       Cast([click-through revenue] AS DECIMAL(38, 2))       as clickthrough_revenue,
+       CASE WHEN Isnumeric([click-through conversions]) = 1 THEN Cast([click-through conversions] AS DECIMAL(20, 10)) ELSE 0 END as clickthrough_conversions,
+       CASE WHEN Isnumeric([click-through transaction count]) = 1 THEN Cast(replace(replace([click-through transaction count],'.0','0'),'.00','0') AS INT) ELSE 0 END as clickthrough_transaction_count
 FROM   [dbo].[dfid041761_ualus_search_floodlight_extracted]
 WHERE  Cast([date] AS DATE) NOT IN (SELECT DISTINCT date
                                     FROM   ualus_search_floodlight)
--- saved_proc/datamart/datamart_scripts/ualus_search_standard_new.sql
+++ saved_proc/datamart/datamart_scripts/ualus_search_standard_new.sql
@@ -6,36 +6,36 @@
 -- comments:
 --====================================================================
 --delete existing records from the fact table (idea is to have rolling 7 days data)
-delete from [ualus_search_standard]
-where  date in (select distinct cast([date] as date)
-                from   [dbo].[dfid041780_ualus_search_standard_extracted])
+DELETE FROM [ualus_search_standard]
+WHERE  date IN (SELECT DISTINCT Cast([date] AS DATE)
+                FROM   [dbo].[dfid041780_ualus_search_standard_extracted])
 
 --transform/cast data types and insert cleansed data into the fact table
-insert into [ualus_search_standard]
-select cast([date] as date)                                  as   date,
-       cast([week] as date)                                  as [week],
-       cast([paid search engine account id] as nvarchar(50)) as PdSearch_EngineAccount_ID,
-       cast([paid search campaign id] as nvarchar(50))       as PdSearch_Campaign_ID,
-       cast([paid search keyword id] as nvarchar(50))        as PdSearch_Keyword_ID,
-       cast([paid search ad group id] as nvarchar(50))       as PdSearch_AdGroup_ID,
-       cast([paid search ad id] as nvarchar(50))             as PdSearch_Ad_ID,
-       cast([paid search advertiser id] as nvarchar(50))     as PdSearch_Advertiser_ID,
-       cast([paid search match type] as nvarchar(50))        as PdSearch_MatchType,
-       cast([site id (dcm)] as nvarchar(50))                 as SiteID_DCM,
-       cast([site id (site directory)] as nvarchar(50))      as SiteID_SiteDirectory,
-       cast([campaign id] as nvarchar(50))                   as Campaign_ID,
-       cast([placement id] as nvarchar(50))                  as Placement_ID,
-       case when isnumeric([paid search impressions]) = 1 then cast([paid search impressions] as bigint) else 0 end                       as PdSearch_Impressions,
-       case when isnumeric([paid search clicks]) = 1 then cast([paid search clicks] as bigint) else 0 end                                 as PdSearch_Clicks,
-       case when isnumeric([paid search cost]) = 1 then cast([paid search cost] as decimal(38, 2)) else 0 end                             as PdSearch_Cost,
-       case when isnumeric([paid search revenue]) = 1 then cast([paid search revenue] as decimal(38, 2)) else 0 end                       as PdSearch_Revenue,
-       case when isnumeric([paid search average position]) = 1 then cast([paid search average position] as decimal(38, 2)) else 0 end     as PdSearch_Avg_Position,
-       case when isnumeric([paid search transactions]) = 1 then cast([paid search transactions] as decimal(38, 2)) else 0 end             as PdSearch_Transactions,
+INSERT INTO [ualus_search_standard]
+SELECT Cast([date] AS DATE)                                  AS date,
+       Cast([week] AS DATE)                                  AS [week],
+       Cast([paid search engine account id] AS NVARCHAR(50)) AS PdSearch_EngineAccount_ID,
+       Cast([paid search campaign id] AS NVARCHAR(50))       AS PdSearch_Campaign_ID,
+       Cast([paid search keyword id] AS NVARCHAR(50))        AS PdSearch_Keyword_ID,
+       Cast([paid search ad group id] AS NVARCHAR(50))       AS PdSearch_AdGroup_ID,
+       Cast([paid search ad id] AS NVARCHAR(50))             AS PdSearch_Ad_ID,
+       Cast([paid search advertiser id] AS NVARCHAR(50))     AS PdSearch_Advertiser_ID,
+       Cast([paid search match type] AS NVARCHAR(50))        AS PdSearch_MatchType,
+       Cast([site id (dcm)] AS NVARCHAR(50))                 AS SiteID_DCM,
+       Cast([site id (site directory)] AS NVARCHAR(50))      AS SiteID_SiteDirectory,
+       Cast([campaign id] AS NVARCHAR(50))                   AS Campaign_ID,
+       Cast([placement id] AS NVARCHAR(50))                  AS Placement_ID,
+       case when isnumeric([paid search impressions]) = 1         then cast(replace(replace([paid search impressions],'.0','0'),'.00','0') as bigint) else 0 end                                                   as pdsearch_impressions,
+       case when isnumeric([paid search clicks]) = 1              then cast(replace(replace([paid search clicks],'.0','0'),'.00','0')      as bigint) else 0 end                                                   as pdsearch_clicks,
+       case when isnumeric([paid search cost]) = 1                then cast([paid search cost] as decimal(38, 2)) else 0 end                                                   as pdsearch_cost,
+       case when isnumeric([paid search revenue]) = 1             then cast([paid search revenue] as decimal(38, 2)) else 0 end                                                   as pdsearch_revenue,
+       case when isnumeric([paid search average position]) = 1    then cast([paid search average position] as decimal(38, 2)) else 0 end                                                   as pdsearch_avg_position,
+       case when isnumeric([paid search transactions]) = 1        then cast([paid search transactions] as decimal(38, 2)) else 0 end                                                   as pdsearch_transactions,
        acquireid
-from   [dbo].[dfid041780_ualus_search_standard_extracted]
-where  cast([date] as date) not in (select distinct [date]
-                                    from   ualus_search_standard)
+FROM   [dbo].[dfid041780_ualus_search_standard_extracted]
+WHERE  Cast([date] AS DATE) NOT IN (SELECT DISTINCT [date]
+                                    FROM   ualus_search_standard)
 
-select max([date]) activitydate
-into   #dt
-from   ualus_search_standard
+SELECT Max([date]) activitydate
+INTO   #DT
+FROM   ualus_search_standard
