41a76e286f0122cfb48099e7880f183c249c0281
matthew.hartwick@NYCMECMHARTWICK <mrhartwick@gmail.com>
2017-08-30 10:50:33 -0400

    updates

--- saved_proc/datamart/datamart_scripts/ualus_ft_extractScript.sql
+++ saved_proc/datamart/datamart_scripts/ualus_ft_extractScript.sql
@@ -1,5 +1,5 @@
 --====================================================================
--- Author:    Matthew Hartwick
+-- Author:      Matthew Hartwick
 -- Create date:      04 Aug 2017 11:47:30 AM ET
 -- Description: {Description}
 -- Comments:
@@ -7,7 +7,8 @@
 IF Object_id(N'DFID060193_FT_Summ_Extracted_Post') IS NOT NULL
   DROP TABLE [DFID060193_FT_Summ_Extracted_Post]
 
-SELECT Cast(Getdate() - 1 AS DATE) AS [Date],
+SELECT Cast(Getdate() AS DATE) AS [Date],
+ DATEADD(day, -1, Getdate())
        t1.*
 INTO   [DFID060193_FT_Summ_Extracted_Post]
 FROM   #DT t1
