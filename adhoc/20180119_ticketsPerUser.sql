fd1c5bff0f4810c1d38f6794943c5989fed3a7aa
matthew.hartwick@NYCMECMHARTWICK <mrhartwick@gmail.com>
2018-01-19 15:43:54 -0500

    only Sojern rtg, add all users - not just those who converted

--- adhoc/20180119_ticketsPerUser.sql
+++ adhoc/20180119_ticketsPerUser.sql
@@ -5,24 +5,21 @@
 
 
 select
-                -- a.user_id,
-                -- a.site_id_dcm,
-                -- conversiontime,
-                -- impressiontime,
---                 count(cvr_nbr),
-                    sum(trans)/count(a.user_id) as tickets_per_user,
-
---                 count(cvr_nbr),
-                    sum(trans)/count(distinct a.user_id) as tickets_per_u_user
+                    sum(trans) as tickets,
+                    count(b.user_id) as users,
+                    count(distinct b.user_id) as unique_users,
+                    sum(trans)/count(b.user_id) as tickets_per_user,
+                    sum(trans)/count(distinct b.user_id) as tickets_per_u_user
                 -- cvr_nbr,
                 -- imp_nbr
 from
-                (select
+    (select
                                 user_id,
                                 -- t1.site_id_dcm,
-                                interaction_time                            as conversiontime,
-                                total_conversions as trans,
-                                row_number() over ()                        as cvr_nbr
+--                                 interaction_time                            as conversiontime,
+                                sum(total_conversions) as trans
+--                                 row_number() over ()                        as cvr_nbr
+--                              count(*) as trans
                 from
                                 diap01.mec_us_united_20056.dfa2_activity    as t1
                 -- these JOINs increase runtime significantly, but we need placement names to filter Sojern's tactics
@@ -31,19 +28,23 @@ from
                                 on t1.placement_id = p1.placement_id
                 where
                                 t1.activity_id = 978826 and
+                                t1.campaign_id = 20606595 and
                                 user_id != '0' and
-                                t1.site_id_dcm = 1239319 and
+                                (t1.site_id_dcm = 1239319 and regexp_like(placement,'.*_RON_.*','ib')) and
 
                                 cast(timestamp_trunc(to_timestamp(interaction_time / 1000000),'SS') as date) between '2018-01-01' and '2018-01-12' and
                                 advertiser_id <> 0 and
                                 (length(isnull (event_sub_type,'')) > 0)
-                ) as a,
+                group by user_id
 
-                (select
+union all
+
+                select
                                 user_id,
                                 -- t2.site_id_dcm,
-                                event_time                                      as impressiontime
+--                                 event_time                                      as impressiontime
                                 -- row_number() over ()                            as imp_nbr
+                    0 as trans
                 from
                                 diap01.mec_us_united_20056.dfa2_impression      as t2
                 left join
@@ -51,15 +52,18 @@ from
                                 on t2.placement_id = p1.placement_id
                 where
                                 user_id != '0' and
-                                -- filter results to Prospecting tactics only
-                                t2.site_id_dcm = 1239319 and
+                                (t2.site_id_dcm = 1239319 and regexp_like(placement,'.*_RON_.*','ib')) and
+                                t2.campaign_id = 20606595 and
                                 cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) between '2018-01-01' and '2018-01-12' and
                                 advertiser_id <> 0
+        group by user_id
                 ) as b
-where
-                a.user_id = b.user_id and
-                -- DCM lookback window is set to 7 days, so interaction_time and event_time already match in the log files
-                conversiontime = impressiontime
+-- where
+--                 a.user_id = b.user_id and
+--                 -- DCM lookback window is set to 7 days, so interaction_time and event_time already match in the log files
+--                 conversiontime = impressiontime
 
 
 
+select count(distinct user_id)
+from
