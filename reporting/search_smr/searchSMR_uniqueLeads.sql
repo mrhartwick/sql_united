978a4979c6ba719afe1f5e6562d95eff9031ca2a
matthew.hartwick@NYCMECMHARTWICK <mrhartwick@gmail.com>
2018-03-06 11:04:43 -0500

    small changes

--- reporting/search_smr/searchSMR_uniqueLeads.sql
+++ reporting/search_smr/searchSMR_uniqueLeads.sql
@@ -1,5 +1,4 @@
--- Insert latest Search metadata into Vertica table
-copy diap01.mec_us_united_20056.ual_dfa2_paid_search_meta from local 'C:\Users\matthew.hartwick\Documents\paid_search_match_20171001-20171031.csv' with DELIMITER ',' DIRECT commit;
+copy diap01.mec_us_united_20056.ual_dfa2_paid_search_meta from local 'C:\Users\matthew.hartwick\Documents\paid_search_match_20180101_20180131.csv' with DELIMITER ',' DIRECT commit;
 
 -- drop existing uniques table
 drop table diap01.mec_us_united_20056.ual_dfa2_paid_search_meta_u;
@@ -44,27 +43,29 @@ paid_search_campaign,paid_search_ad_group,paid_search_bid_strategy,paid_search_l
 commit;
 
 
--- pull unique leads at desired level
 
 select
 --     ta.user_id,
---  ta.date,
+ ta.date,
         ta.week as week,
-    -- ta.advertiser,
---  ta.campaign,
-    -- ta.site_dcm,
+    ta.advertiser,
+ ta.campaign,
+    ta.site_dcm,
     -- ta.site_id_dcm    as siteid_dcm,
---     ta.paid_search_keyword,
+
+    ta.paid_search_keyword,
+    ta.segment_value_1 as keyword_id,
 --     ta.keyword_id as keyword_id,
 --     ta.ad_id,
-    -- ta.paid_search_campaign,
+    ta.paid_search_campaign,
     ta.paid_search_ad_group,
 --  ta.paid_search_bid_strategy,
---  sum(thing)        as thing,
+ sum(thing)        as thing,
     count(distinct ta.user_id) as u_leads,
 --     count(r1) as r1,
---  count(r2)         as r2,
-    sum(tot_con)      as tot_con
+ count(r2)         as r2,
+    sum(tot_con)      as tot_con,
+sum(leads) as leads
 --     sum(ta.r1) as row_count
 
 from
@@ -75,11 +76,11 @@ from
             a.segment_value_1,
             a.ad_id,
             a.site_id_dcm,
---          c1.campaign,
+         c1.campaign,
             d1.site_dcm,
             a1.advertiser,
-            cast(timestamp_trunc(to_timestamp(a.interaction_time / 1000000),'SS') as date) as date,
-            date_trunc('week', timestamp_trunc(to_timestamp(a.interaction_time / 1000000),'SS')) -1 as week,
+            cast(timestamp_trunc(to_timestamp(a.event_time / 1000000),'SS') as date) as date,
+            date_trunc('week', timestamp_trunc(to_timestamp(a.event_time / 1000000),'SS')) -1 as week,
             row_number() over ()                                                         as r2,
             1                                                                            as thing,
             count(*)                                                                     as leads,
@@ -101,25 +102,29 @@ from
             left join diap01.mec_us_united_20056.dfa2_advertisers as a1
             on a.advertiser_id = a1.advertiser_id
 
+            left join diap01.mec_us_united_20056.dfa2_campaigns as c1
+            on a.campaign_id = c1.campaign_id
+
 -- left join diap01.mec_us_united_20056.dfa2_campaigns as c1
 -- on a.campaign_id = c1.campaign_id
 
-        where cast(timestamp_trunc(to_timestamp(a.interaction_time / 1000000),'SS') as date) between '2017-12-01' and '2017-12-31'
-            and (a.activity_id = 1086066)
-            and (a.advertiser_id <> 0)
+        where cast(timestamp_trunc(to_timestamp(a.event_time / 1000000),'SS') as date) between '2018-01-01' and '2018-01-31'
+            and a.activity_id = 1086066
+            and a.advertiser_id <> 0
             and (length(isnull (a.event_sub_type,'')) > 0)
+            and a.user_id <> '0'
 --          and a.campaign_id = 8063775
         group by
             a.user_id,
             a.segment_value_1,
             a.ad_id,
-            cast(timestamp_trunc(to_timestamp(a.interaction_time / 1000000),'SS') as date),
-            date_trunc('week', timestamp_trunc(to_timestamp(a.interaction_time / 1000000),'SS')),
---  date_trunc('week', timestamp_trunc(to_timestamp(interaction_time / 1000000),'SS')),
+            cast(timestamp_trunc(to_timestamp(a.event_time / 1000000),'SS') as date),
+            date_trunc('week', timestamp_trunc(to_timestamp(a.event_time / 1000000),'SS')),
+--  date_trunc('week', timestamp_trunc(to_timestamp(event_time / 1000000),'SS')),
             s1.paid_search_keyword_id,
             a.site_id_dcm,
             s1.paid_search_campaign,
---   c1.campaign,
+  c1.campaign,
             s1.paid_search_keyword,
             d1.site_dcm,
 --          s1.paid_search_bid_strategy,
@@ -127,12 +132,19 @@ from
             s1.paid_search_ad_group
 
     ) as ta
-where ta.advertiser = 'United_Search'
+--     where ta.segment_value_1 != 0
+--     and (length(isnull(cast(ta.segment_value_1 as varchar),'')) > 0)
+
+-- where ta.advertiser = 'United_Search'
 group by
---  ta.date,
+ ta.date,
+    ta.paid_search_keyword,
+    ta.paid_search_campaign,
     ta.week,
---  ta.campaign,
+--     ta.keyword_id,
+    ta.segment_value_1,
+ ta.campaign,
     -- ta.site_id_dcm,
-    -- ta.advertiser,
-    -- ta.site_dcm,
+    ta.advertiser,
+    ta.site_dcm,
     ta.paid_search_ad_group
