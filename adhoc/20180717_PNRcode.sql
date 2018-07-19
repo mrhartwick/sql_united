096f4c14ad0688d92c26f062f8892cb2e4505b7a
thahmed <tammam.ahmed@mecglobal.com>
2018-07-19 11:02:35 -0400

    md_event_date instead for booking date

--- adhoc/20180717_PNRcode.sql
+++ adhoc/20180717_PNRcode.sql
@@ -1,15 +1,9 @@
-
-
-
 select
---    cast(date_part(month, (t1.md_interaction_date_loc)) as int) as reportmonth
-      md_interaction_time_loc as booking_time
-    , md_interaction_date_loc                             as booking_date
-    , cast(replace(replace(regexp_substr(other_data,'(u9\\=)(\\d.?\\d.?\\d.?\\d.?\\d.?\\d.?\\d.?\\d)(\\;u10\\=)'),'u9=',''),';u10=','') as date) as traveldate_1
+     md_event_date_loc                             as booking_date
     , Left(Right(other_data, Len(other_data) - Position('u18=' in other_data) - 3), Position(';' in Right(other_data, Len(other_data) - Position('u18=' in other_data) - 3)) - 1) as BASE64_PNR
     , udf_base64Decode(Left(Right(other_data, Len(other_data) - Position('u18=' in other_data) - 3), Position(';' in Right(other_data, Len(other_data) - Position('u18=' in other_data) - 3)) - 1)) as PNR
     , sum(total_conversions)                              as Tickets
---       ,p1.placement
+--     ,p1.placement
 from
     united.dfa2_activity as t1
 
@@ -24,11 +18,9 @@ where
 --        and p1.placement like '%PROS_FT%'
 --        and p1.placement like '%RouteSupport%'
         and total_revenue <> '0'
-        and t1.campaign_id in (20561981, 8063775, 9917294, 8161352, 9961760, 6146233)
---        and t1.site_id_dcm = 3267410
+        and t1.campaign_id in (20713692, 21230517)
+--        and t1.site_id_dcm in (1190273, 1239319)
 group by
     other_data,
-    md_interaction_date_loc,
---  cast(date_part(month, (t1.md_interaction_date_loc)) as int),
-    md_interaction_time_loc
-
+     md_event_date_loc
+-- 	p1. placement
