c934b2c58af3c01260e55911610a2f302149a49e
thahmed <tammam.ahmed@mecglobal.com>
2018-08-10 14:30:39 -0400

    changed code a bit with redshift shift

--- adhoc/ft_integration/fare-no fare code.sql
+++ adhoc/ft_integration/fare-no fare code.sql
@@ -1,10 +1,10 @@
----Match conversion data with impression table using user_id/timestammp 
+---Match conversion data with impression table using user_id/timestammp
 
-create table diap01.mec_us_united_20056.ual_fare_tbl_3
+create table wmprodfeeds.united.ual_fare_tbl_3
 (
 user_id           varchar(50),
-con_tim           int,
-imp_tim           int,
+con_date           date,
+imp_date           date,
 campaign_id       int,
 site_id           int,
 placement_id      int,
@@ -14,10 +14,12 @@ tix               int,
 rev               decimal(20,10)
 );
 
-insert into  diap01.mec_us_united_20056.ual_fare_tbl_3
+
+
+insert into  wmprodfeeds.united.ual_fare_tbl_3
 (user_id,
-con_tim,
-imp_tim,
+con_date,
+imp_date,
 campaign_id,
 site_id,
 placement_id,
@@ -27,10 +29,10 @@ tix,
 rev)
 
     (
-    select
+ select
        ta.user_id
-      ,ti.contim as con_tim
-      ,ta.eventtime as imp_tim
+      ,ti.con_date as con_date
+      ,ta.imp_date as imp_date
       ,ta.campaign_id
       ,ta.site_id
       ,ta.placement_id
@@ -42,20 +44,24 @@ rev)
         from
         (
             select
-            event_time as eventtime,
+            event_time as impressiontime,
+            md_event_date_loc as imp_date,
             row_number() over() as imps,
             campaign_id as campaign_id,
             site_id_dcm as site_id,
-            placement_id as placement_id,
-            substring(u_value,(instr(u_value,'U=') + 2),7) as creative_ver_id,
+            t1.placement_id as placement_id,
+            substring(u_value,3,7) AS creative_ver_id,
             user_id as user_id
 
-            from diap01.mec_us_united_20056.dfa2_impression
-            where cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) between '2018-04-25' and '2018-05-18'
+            from wmprodfeeds.united.dfa2_impression as t1
+
+            where md_event_date_loc between '2018-06-27' and '2018-07-31'
             and campaign_id = 20606595 -- GM ACQ 2018
             and (advertiser_id <> 0)
             and site_id_dcm = 1239319
             and user_id <> '0'
+
+
 
             ) as ta,
 
@@ -63,43 +69,44 @@ rev)
             select
             a.user_id,
             a.interaction_time as contim,
+            a.md_interaction_date_loc as con_date,
             total_conversions as tix,
-            (total_revenue * 1000000)/rates.exchange_rate as rev
+            case when regexp_instr(substring(other_data,(regexp_instr(other_data,'u3\\=') + 3),3),'Mil.*') = 0 then cast(((a.total_revenue * 1000000) / (rates.exchange_rate)) as decimal(20,10))
+            when regexp_instr(substring(other_data,(regexp_instr(other_data,'u3\\=') + 3),3),'Mil.*') > 0 then cast(((a.total_revenue*1000)/.0103) as decimal(20,10)) end as rev
 
 
-            from diap01.mec_us_united_20056.dfa2_activity a
+            from wmprodfeeds.united.dfa2_activity a
 
-            left join diap01.mec_us_mecexchangerates_20067.exchange_rates as rates
-            on upper(substring(a.other_data,(instr(a.other_data,'u3=') + 3),3)) = upper(rates.currency)
-            and cast(timestamp_trunc(to_timestamp(a.event_time / 1000000),'SS') as date) = rates.date
+            left join wmprodfeeds.exchangerates.exchange_rates as rates
+           on upper(substring(other_data,(regexp_instr(other_data,'u3\\=') + 3),3)) = upper(rates.currency)
+           and md_event_date_loc = rates.date
 
-            where cast(timestamp_trunc(to_timestamp(interaction_time / 1000000),'SS') as date) between '2018-04-25' and '2018-05-18'
-            and not regexp_like(substring(other_data,(instr(other_data,'u3=') + 3),5),'mil.*','ib')
+            where md_interaction_date_loc between '2018-06-27' and '2018-07-31'
             and activity_id = 978826
             and campaign_id = 20606595 -- GM ACQ 2018
             and (advertiser_id <> 0)
             and user_id <> '0'
-            and (length(isnull (event_sub_type,'')) > 0)
+            and (length(isnull(event_sub_type,'')) > 0)
             and site_id_dcm = 1239319
             and total_revenue <> 0
+--             and conversion_id in (1,2)
             ) as ti
 
       where
       ta.user_id = ti.user_id
-      and eventtime = contim
+      and impressiontime = contim
       );
 
 commit;
-
 --=========================================================================================================================================================
 
 --Step 2: Query to pull in placement level data
 
 select
-    t3.dcmdate               as "Date",
+    cast(t3.dcmdate as date)                                                   as "date",
     t3.placement             as placement,
     t3.creative_ver_id       as creative_ver_id,
-    sum(t3.impressions)      as imps,
+--     sum(t3.impressions)      as imps,
     sum(t3.tix)              as tix,
     sum(t3.rev)              as rev
 
@@ -131,48 +138,37 @@ from
 -- openquery function call must not exceed 8,000 characters; no room for comments inside the function
         from (
                  select *
-                 from openQuery(verticaunited,
+                 from openQuery(redshift,
                                 '
                                 select
-                                cast(timestamp_trunc(to_timestamp(con_tim / 1000000),''SS'') as date) as dcmdate,
+                                con_date as dcmdate,
                                 t1.campaign_id,
                                 t1.site_id,
                                 p1.placement,
                                 t1.placement_id,
-                                count(imps) as impressions,
+                                count(distinct imps) as impressions,
                                 t1.creative_ver_id,
                                 sum(tix) as tix,
                                 sum(rev) as rev
-                                from diap01.mec_us_united_20056.ual_fare_tbl_3 as t1
+                                from wmprodfeeds.united.ual_fare_tbl_3 as t1
 
 
-                                left join
+                               left join
                                 (
-                                select
-                                cast(p1.placement as varchar(4000)) as ''placement'',p1.placement_id as ''placement_id'',p1.campaign_id as ''campaign_id'',p1.site_id_dcm as ''site_id_dcm''
-
-                                from (select
-                                campaign_id as campaign_id,site_id_dcm as site_id_dcm,placement_id as placement_id,placement as placement,cast(placement_start_date as date) as thisdate,
-                                row_number() over ( partition by campaign_id,site_id_dcm,placement_id
-                                order by cast(placement_start_date as date) desc ) as x1
-                                from diap01.mec_us_united_20056.dfa2_placements
-
-                                ) as p1
-                                where x1 = 1
+                                select cast (placement as varchar (4000)) as placement,placement_id as placement_id
+                                from wmprodfeeds.united.dfa2_placements
                                 ) as p1
                                 on t1.placement_id = p1.placement_id
-                                and t1.campaign_id = p1.campaign_id
-                                and t1.site_id = p1.site_id_dcm
 
 
-                                where not regexp_like(p1.placement,''.*_PROS_FT.*'',''ib'')
+                                where p1.placement not like ''%PROS_FT%'' or p1.placement not like ''%Route%''
                                 and t1.site_id = 1239319
                                 and t1.campaign_id = 20606595
-                                and cast(timestamp_trunc(to_timestamp(con_tim / 1000000),''SS'') as date) between ''2018-04-25'' and ''2018-05-18''
+                                and con_date between ''2018-04-25'' and ''2018-07-27''
 
                                 group by
 
-                                cast(timestamp_trunc(to_timestamp(con_tim / 1000000),''SS'') as date),
+                                con_date,
                                 t1.campaign_id,
                                 t1.site_id,
                                 t1.placement_id,
@@ -202,10 +198,10 @@ group by
     t3.placement
 
 --=========================================================================================================
---To pull in imps & clicks 
+--To pull in imps & clicks
 
 select
-cast(timestamp_trunc(to_timestamp(t01.event_time / 1000000),'SS') as date) as "date",
+t01.date as "date",
 p1.placement as placement,
 t01.creative_ver_id as creative_ver_id,
 sum(t01.impressions) as impressions,
@@ -213,64 +209,68 @@ sum(t01.clicks) as clicks
 
 from (
 select
-ti.event_time                                                             as event_time,
+ti.md_event_date_loc                                                             as "date",
 ti.campaign_id                                                            as campaign_id,
 ti.site_id_dcm                                                            as site_id_dcm,
 ti.placement_id                                                           as placement_id,
-substring(u_value,(instr(u_value,'U=') + 2),7)                            as creative_ver_id,
+substring(u_value,3,7) AS creative_ver_id,
 count(*)                                                                  as impressions,
 0                                                                         as clicks
 
 from (
 select *
-from diap01.mec_us_united_20056.dfa2_impression
-where cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) between '2018-04-25' and '2018-05-18'
+from wmprodfeeds.united.dfa2_impression
+where md_event_date_loc between '2018-04-25' and '2018-07-27'
 and (advertiser_id <> 0)
 
 ) as ti
 group by
 ti.campaign_id,
 ti.site_id_dcm,
-ti.event_time,
-substring(u_value,(instr(u_value,'U=') + 2),7),
+ti.md_event_date_loc,
+substring(u_value,3,7),
 ti.placement_id
 
 union all
 
 select
-tc.event_time                                                             as event_time,
+tc.md_event_date_loc                                                             as "date",
 tc.campaign_id                                                            as campaign_id,
 tc.site_id_dcm                                                            as site_id_dcm,
 tc.placement_id                                                           as placement_id,
-substring(u_value,(instr(u_value,'U=') + 2),7)                            as creative_ver_id,
+substring(u_value,3,7) AS creative_ver_id,
 0                                                                         as impressions,
 count(*)                                                                  as clicks
 
 from (
 
 select *
-from diap01.mec_us_united_20056.dfa2_click
-where cast(timestamp_trunc(to_timestamp(event_time / 1000000),'SS') as date) between '2018-04-25' and '2018-05-18'
+from wmprodfeeds.united.dfa2_click
+where md_event_date_loc between '2018-04-25' and '2018-07-27'
 and (advertiser_id <> 0)
 
 ) as tc
 
 group by
-substring(u_value,(instr(u_value,'U=') + 2),7),
+substring(u_value,3,7),
 tc.campaign_id,
-tc.event_time,
+tc.md_event_date_loc,
 tc.site_id_dcm,
 tc.placement_id
 ) as t01
 
-left join diap01.mec_us_united_20056.dfa2_placements as p1
+ left join
+(
+select cast (placement as varchar (4000)) as placement,placement_id as placement_id
+from wmprodfeeds.united.dfa2_placements
+) as p1
 on t01.placement_id = p1.placement_id
 where t01.campaign_id = 20606595
 and t01.site_id_dcm = 1239319
-and not regexp_like(p1.placement,'.*_PROS_FT.*','ib')
+and p1.placement not like '%PROS_FT' OR p1.placement not like '%Route%'
 
 
 group by
-cast(timestamp_trunc(to_timestamp(t01.event_time / 1000000),'SS') as date),
+t01.date,
 p1.placement,
 t01.creative_ver_id
